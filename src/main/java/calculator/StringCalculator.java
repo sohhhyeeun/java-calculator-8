9210006cc1a6f4be76e3b6ebaf4c57b945cdf30b
sohhhyeeun <syeankm12076@gmail.com>
2025-10-17 21:07:03 +0900

    feat: 빈 문자열 또는 null을 입력할 경우, 0 반환

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringCalculator {
+    public int add(String str) {
+        if (str == null || str.isEmpty()) {
+            return 0;
+        }
+
+        return 1; //임시 반환값
+    }
+}
