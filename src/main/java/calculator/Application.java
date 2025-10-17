967997659403e64863cfb7806fa9f6a249a0b19a
sohhhyeeun <syeankm12076@gmail.com>
2025-10-17 23:31:37 +0900

    feat: 커스텀 구분자 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,6 +8,8 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
+        input = input.replace("\\n", "\n");
+
         StringCalculator stringCalculator = new StringCalculator();
         int output = stringCalculator.add(input);
 
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,13 +1,26 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public int add(String str) {
         if (str == null || str.isEmpty()) {
             return 0;
         }
 
+        String delimiter = "[,:]";
+        String numString = str;
+
+        Matcher matcher = Pattern.compile("//(.)\\n(.*)").matcher(str);
+        if (matcher.find()) {
+            delimiter = matcher.group(1);
+            numString = matcher.group(2);
+        }
+
+        String[] nums = numString.split(delimiter);
+
         int sum = 0;
-        String[] nums = str.split("[,:]");
         for (String num : nums) {
             sum += Integer.parseInt(num);
         }
